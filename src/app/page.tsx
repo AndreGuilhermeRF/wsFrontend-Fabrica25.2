5fdbae6ab4aef0d8ec3e0780a42c75b904df2781
AndreGuilhermeRF <andreguilherme3d1@gmail.com>
2025-09-14 11:05:31 -0300

    Teste para Vercel 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import "./globals.css";
 import { Header } from "@/components/Header/page";
 import { Footer } from "@/components/Footer/page";
+import Image from "next/image";
 
 export default function Home() {
   const [pokemon, setPokemon] = useState([]);
@@ -12,19 +13,18 @@ export default function Home() {
     getPoke();
   }, []);
   const getPoke = async () => {
-    const endpoints = [];
-    for (let i = 1; i < 152; i++) {
+    var endpoints = [];
+    for (var i = 1; i < 152; i++) {
       endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
     }
     axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemon(res));
   };
   const filterPoke = (name) => {
-    const filteredPokemon = [];
+    var filteredPokemon = [];
     if (name === "") {
       getPoke();
-      return;
     }
-    for (let i in pokemon) {
+    for (var i in pokemon) {
       if (pokemon[i].data.name.includes(name)) {
         filteredPokemon.push(pokemon[i]);
       }
@@ -42,7 +42,16 @@ export default function Home() {
             image={poke.data.sprites.front_default}
             id={poke.data.id}
             key={key}
-          />
+          >
+            <Image
+              src={poke.data.sprites.front_default}
+              alt={poke.data.name}
+              width={120}
+              height={120}
+              className="details-image"
+            />
+            <div>{poke.types.map((t: { type: { name: string } }) => t.type.name).join(", ")}</div>
+          </PokemonCard>
         ))}
       </div>
       <Footer />
--- src/components/PokemonCard/page.tsx
+++ src/components/PokemonCard/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 import * as React from 'react';
 import Link from "next/link";
-
+import Image from 'next/image';
 
 export default function PokemonCard({ name, image, id, isGrid = false }) {
   return (
     <Link href={`/details/${id}`}>
       <div className="pokemon-card">
-        <img
+        <Image
           src={image}
-          alt={name}   
+          alt={name}
+          width={96}
+          height={96}
         />
         <div className="pokemon-name">
           <div className="pokemon-type"><p className="font-bold capitalize">{name}</p></div>
